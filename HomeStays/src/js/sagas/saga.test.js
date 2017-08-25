dae4c9a22b66e21e9d754032b437a07f16331e83
PRIYANKA KAROLIA <PRIK@saxobank.com>
2017-08-25 12:38:19 +0530

    update

--- HomeStays/src/js/sagas/saga.test.js
+++ HomeStays/src/js/sagas/saga.test.js
@@ -4,17 +4,20 @@ import * as actionTypes from '../constants/actionTypes';
 import { call, put, takeEvery, race, all } from 'redux-saga/effects';
 import apiHelper from '../common/apiHelper';
 
-describe('saga', () => {
-    describe('test fetchCards', () => {
-        let timer;
-        let it= sagaHelper(saga.fetchCards());;
 
-        beforeAll(() => {  
+describe('saga', () => {
+    let timer
+    beforeAll(() => {
+        timer = jest.fn().mockImplementation(() => new Promise(function (resolve) {}));
+    })
+    describe('test fetchCards', () => {
+        let it = sagaHelper(saga.fetchCards());;
+
+        beforeAll(() => {
             apiHelper.getAllCards = jest.fn().mockReturnValue(true);
-            timer = jest.fn().mockImplementation(() => new Promise(function (resolve) {}));
         })
 
-        it('should have dispatched fetching action', result=>{
+        it('should have dispatched fetching action', result => {
             expect(result).toEqual(put({
                 type: actionTypes.FETCHING
             }));
@@ -25,20 +28,33 @@ describe('saga', () => {
                 data: call(() => apiHelper.getAllCards()),
                 timeout: timer()
             })));
-            return {name: 'Jaipur'};
+            return {
+                name: 'Jaipur'
+            };
+        })
+    })
+
+    describe('test fetchCardDetail', () => {
+        let it = sagaHelper(saga.fetchCardDetail());;
+
+        beforeAll(() => {
+            apiHelper.getCardDetails = jest.fn().mockReturnValue(true);
         })
 
-        it('should dispatch success message', result=> {
-            expect(result).toEqual({ type: actionTypes.FETCH_CARDS_SUCCESS, value: true });
+        it('should have dispatched fetching action', result => {
+            expect(result).toEqual(put({
+                type: actionTypes.FETCHING
+            }));
         })
 
-        // it('testing fetchCards third yield statement', () => {
-        //     let fetchCards = saga.fetchCards();
-        //     apiHelper.getAllCards = jest.fn(true);
-
-        //     expect(fetchCards.next().value).toEqual(put({
-        //         type: actionTypes.FETCHING
-        //     }));
-        // })
+        it('should have called the api', result => {
+            expect(JSON.stringify(result)).toEqual(JSON.stringify(race({
+                data: call(() => apiHelper.getAllCards()),
+                timeout: timer()
+            })));
+            return {
+                name: 'Jaipur'
+            };
+        })
     })
 })
